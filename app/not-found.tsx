ccfc9e0d86465fd2406c6d91081f41fbe5637752
omar-taha <ot486061@gmail.com>
2025-12-10 08:28:18 +0200

    update next version and solve building issue

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import { Box, Typography, Button } from "@mui/material";
 
--- components/WelcomeContent.tsx
+++ components/WelcomeContent.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Box, Typography, Button } from "@mui/material";
 import Link from "next/link";
 
